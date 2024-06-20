0e21ae71d53f018dcd985b3d4c88265261004ea3
soohoo <thuongshiro1102@gmail.com>
2024-06-20 16:15:08 +0900

    Question1

---
+++ src/Question1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Question1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        System.out.println(line);
+        System.out.println(line);
+        System.out.println(line);
+    }
+}
---
+++ src/Question2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Question2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(b);
+        System.out.print(a);
+    }
+}
