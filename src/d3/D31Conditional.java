05c8d08b1c19fede625ab9030106eea534245d8e
soohoo <thuongshiro1102@gmail.com>
2024-06-25 10:07:54 +0900

    자료

--- src/d3/D31Conditional.java
+++ src/d3/D31Conditional.java
@@ -1,2 +1,110 @@
-package d3;public class D31Conditional {
+package d3;
+
+public class D31Conditional {
+    public static void main(String[] args) {
+        int age = 10;
+        if ( age < 20) {
+            System.out.println("You cannot enter");
+        }
+        // 비교, 논리 연산자가 많이 활룔되며
+        if (true) System.out.println("always run");
+
+        int num = 7;
+        // num의 짝수이면 event이라고 출력하고 싶다.
+        if (num % 2 == 0) {
+            System.out.println("even");
+        }
+
+        // 4의 배수인지 확인한 다음
+        // 4의 배수가 맞으면 100의 배수인지
+        // 100의 배수가 맞으면 400의 배수인지
+        int year = 2024;
+        if (year % 4 ==0) {
+            System.out.println("4의 배수");
+            if (year % 100 == 0) {
+                System.out.println("100의 배수");
+                // 4의 배수이면서 100의 배수일때만 실행되는 코드
+            }
+            if (year % 400 == 0) {
+                // 4의 배수이면서 4-00의 배수일때만 실행되믄 코드
+                System.out.println("400의 배수");
+            }
+        }
+
+        // 느긋하면(남은시간 1시간 이상) 버스, 급하면 택시
+        int current = 8;
+        int goal = 9;
+        if (goal - current >= 2) {
+            System.out.println("Take Bus");
+        }
+        else if (goal - current >= 1) {
+            System.out.println("Take Subway");
+        }
+        else {
+            System.out.println("Take Cab");
+        }
+
+
+        int dust = 62;
+        if (dust <= 30) {
+            System.out.println("Good");
+        }
+        else if(dust <= 80) {
+            System.out.println("Bad");
+        }
+        else if (dust <= 150) {
+            System.out.println("Normal");
+        }
+        else {
+            System.out.println("Very Bad");
+        }
+
+        // 만약 앞선 조건이 true이 되면 아래는 실행되지 않다.
+        int someNum = 0;
+        if (someNum == 0) {
+            System.out.println("someNum is 0");
+        }
+        else if (10 % someNum == 0) {
+            System.out.println("someNum is factor of 10");
+        }
+        else {
+            System.out.println("someNum is not factor f 10");
+        }
+
+        // 만약에 검사하고 싶은 조건이 a == <특정 값>으로 이뤄져 있으면?
+        char input = 'w';
+        if (input == 'w') {
+            System.out.println("up");
+        }
+        else if (input == 's') {
+            System.out.println("down");
+        }
+        else if (input == 'd') {
+            System.out.println("right");
+        }
+        else if (input == 'a') {
+            System.out.println("left");
+        }
+
+        // 이럴때는 switch - case를 쓸 수 있다.
+        // 괄호 안에 대상 변수를 넣어준다.
+        input = 's';
+        switch (input) {
+            case 'w':  // 콜론(:)
+                System.out.println("up");
+                break;
+            case 's':  // 콜론(:)
+                System.out.println("down");
+                break;
+            case 'd':  // 콜론(:)
+                System.out.println("right");
+                break;
+            case 'a':  // 콜론(:)
+                System.out.println("left");
+                break;
+            default:
+                System.out.println("Not a direction");
+        }
+        // 다르게 말하면 모든 if 조건이 '=='으로 이뤄진 if를 대신할 수 있다.
+    }
 }
--- src/d3/D32Loop.java
+++ src/d3/D32Loop.java
@@ -1,2 +1,58 @@
-package d3;public class D32Loop {
+package d3;
+
+import java.util.Scanner;
+
+public class D32Loop {
+    public static void main(String[] args) {
+        // 대출금을 갚자
+        int loan = 1000;
+        // 매달 갚을 금액
+        int monthly = 50;
+        // if는 내부의 조건이 true 일떄 실행
+        // while은 내부의 true 일동안 실행
+        int months = 0;
+        // 몇 달을 걸렸는지
+        while (loan > 0) {
+            // while 내부에서도 if , else if, slse 가능
+            if (loan < monthly) {
+                loan = 0;
+            }
+            loan -= monthly;
+            loan *= 1.03;
+            System.out.println(String.format("Remaining: %d", loan));
+            months++; // months = months + 1
+        }
+        System.out.println(months);
+        System.out.println(loan);
+
+
+        // 로그인 횟수를 세가며 반복 해보자
+        Scanner scanner = new Scanner(System.in);
+        // 맞춰야 되는 비밀번호
+        int password = 1234;
+        // 시도한 횟수를 기록하는 정수 변수
+        int tries = 0;
+        while (!(tries > -1)) {
+            // 사용자가 비밀번호를 입력한다.
+            System.out.print("Input Password: ");
+            int input = scanner.nextInt();
+            if (password == input) {
+                // 나중에 넣어보기
+            }
+            tries++;
+        }
+        System.out.println("False to login");
+
+        // for
+        // 시작하는 저건, 끝나는 조건, 한번마다 실행할 코드
+        for (int i = 0; i < 5; i++) {
+            System.out.println("i: " + i);
+        }
+        System.out.println("끝");
+
+        // intellij에서 fori + TAB
+        for (int i = 0; i < 10; i++) {
+            System.out.println("i ^ 2: " + (i * i));
+        }
+    }
 }
