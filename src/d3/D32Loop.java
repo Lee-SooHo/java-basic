c119f51d9c0d725cd72fd732bdfb63901f7aaa11
soohoo <thuongshiro1102@gmail.com>
2024-06-25 13:45:29 +0900

    자료

--- src/d3/D32Loop.java
+++ src/d3/D32Loop.java
@@ -54,5 +54,66 @@ public class D32Loop {
         for (int i = 0; i < 10; i++) {
             System.out.println("i ^ 2: " + (i * i));
         }
+
+        int[] numbers = {1, 2, 3, 4, 5};
+// i를 쓰는 대신 배열의 원소를 하나씩 가져오자. -> for each
+        for (int number : numbers) {
+            // number에 numbers의 원소가 순서대로 할당된다.
+            System.out.println("number: " + number);
+        }
+/*for (int i = 0; i < numbers.length; i++) {
+    int number = numbers[i];
+    System.out.println("number: " + number);
+}*/
+
+// 문자열 배열
+        String[] names = {"alex", "brad", "chad", "dave", "eric", "fred", "greg"};
+// for each를 사용하면?
+        for (String name : names) {
+            System.out.println("name: " + name);
+        }
+
+        // 잔고와 매월 사용 금액이 주어질때
+        // 잔고가 0이되면 멈추자
+        int price = 50;
+        int savings = 200;
+        // 10번 반복하는 for문
+        for (int i = 0; i < 10; i++) {
+            // 매월 금액 차감
+            savings -= price;
+            if (savings - price < 0) {
+                System.out.println("잔고가 부족합니다.");
+                // break: 반복을 멈추고 반복문에서 나간다.
+                break;
+            }
+        }
+        System.out.println(savings);
+
+        // 숫자 배열 중, 양수 만 합치고 싶다.
+        int[] accounts = {100, 20, -40, 60, -80, 100};
+        int sum = 0;
+        for (int account : accounts) {
+            // continue: 이번 반복을 중단하고, 다음 반복으로 넘어가자.
+            // continue 이후의 남은 코드는 실행하지 않는다.
+            if (account < 0) continue;
+            sum += account;
+        }
+        System.out.println("sum: " + sum);
+
+        // 중첩 반복에서 break 또는 continue를 사용할 때는
+        // 제일 가까운 반복문만 영향을 받음
+        while (true) {
+            for (int i = 0; i < 10; i++) {
+                while (true) {
+                    // 이 break는 130번 줄의 while을 종료
+                    break;
+                }
+                if (i > 5) {
+                    // 이 break는 129번 줄의 for를 종료
+                    break;
+                }
+            }
+            break;
+        }
     }
 }
